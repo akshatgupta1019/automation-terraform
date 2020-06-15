c96e3e9e73bcf2e1515415aed274048a91317ec1
akshatgupta1019 <50566056+akshatgupta1019@users.noreply.github.com>
2020-06-15 15:50:30 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,31 @@
-hello this is akshat gupta
-
-<br/>
-
+<html>
+<head>
+<title>Webhosting using Terraform</title>
+<style>
+body
+{
+background-color:skyblue;
+}
+h1
+{
+color:purple;
+background-color:black;
+color:white;
+padding:10px;
+margin:2em;
+box-shadow:3px 3px 3px 3px yellow;
+}
+</style></head>
+<body>
+<h1>Welcome to My Page</h1>
 <pre>
 <?php
+
 print `ifconfig`;
 
 ?>
+
 </pre>
+<img src=''>
+</body>
+</html>
